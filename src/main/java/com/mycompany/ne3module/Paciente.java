18aebc673b5fbc9e51f3635c11feabbd566aca84
Foxsa <marcosviniciosdasilvainglezros@gmail.com>
2025-09-13 21:11:43 -0300

    First Commit

---
+++ src/main/java/com/mycompany/ne3module/Paciente.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ne3module;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author marco
+ */
+class Paciente {
+    private static int contador1 = 1 ;
+    Scanner sc = new Scanner(System.in);
+    
+    int id;
+    String nome;
+    int idade;
+    String sintoma;
+    
+    public Paciente(String nome, int idade, String sintoma){
+     this.id = contador1++;
+     this.nome = nome;
+     this.idade = idade;
+     this.sintoma = sintoma;
+    }
+    
+    public int getId(){
+        return id;
+}
+    public String getNome(){
+        return nome;
+}
+    
+    public int getIdade(){
+        return idade;
+}
+    
+public String getSintoma(){
+    return sintoma;
+}
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setSintoma(String sintoma) {
+        this.sintoma = sintoma;
+    }
+    
+}
+
+
